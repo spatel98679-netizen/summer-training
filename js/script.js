3f8b60bbd454a1a10f952c671ffafd63dcacc13e
pritam patel <spatel98679@gmail.com>
2026-06-29 17:59:51 +0530

    react

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ const salary = document.getElementById("Employeesalary");
 const role = document.getElementById("EmployeeRole");
 const experience = document.getElementById("EmployeeExperience");
 const submitBtn = document.getElementById("submit");
+const empSection = document.getElementById("empSection");
+
+function renderEmpList(emp) {
+  // emps?.map((emp) => {
+    const empContainer = document.createElement("div");
+    empContainer.classList = `p-2  w-full text-white bg-${emp.id % 2 === 0 ? "red-500" : "orange-500"}`;
+    const nameTag = document.createElement("h3");
+    const no = document.createElement("div");
+    no.classList ="w-10 h-10 rounded-full bg-red-100 text-black flex item-center justify-center pt-2 ";
+    no.innerText = emp.id;
+    const EmailTag = document.createElement("h3");
+    const salaryTag = document.createElement("h3");
+    const roleTag = document.createElement("h3");
+    const experienceTag = document.createElement("h3");
+    nameTag.innerText = "Name : " + emp.name;
+    EmailTag.innerText = "Email : " + emp.email;
+    salaryTag.innerText = "Salary : " + emp.salary;
+    roleTag.innerText = "Role : " + emp.role;
+    experienceTag.innerText = "Experience : " + emp.experience;
+    empContainer.appendChild(no);
+    empContainer.appendChild(nameTag);
+    empContainer.appendChild(salaryTag);
+    empContainer.appendChild(EmailTag);
+    empContainer.appendChild(roleTag);
+    empContainer.appendChild(experienceTag);
+    empSection.prepend(empContainer);
+  // });
+}
 
 // console.log(name);
 
@@ -20,10 +48,53 @@ const submitBtn = document.getElementById("submit");
 //   }
 // });
 
-const empList = [];
+const empList = [
+  // {
+  //   id: 1,
+  //   email: "kumararvind60700@gmail.com",
+  //   experience: "fewrfwe",
+  //   name: "class 1",
+  //   role: "ye3ruerhfou",
+  //   salary: "raj8764654",
+  // },
+  // {
+  //   id: 2,
+  //   email: "kumararvind60700@gmail.com",
+  //   experience: "fewrfwe",
+  //   name: "class 1",
+  //   role: "ye3ruerhfou",
+  //   salary: "raj8764654",
+  // },
+  //  {
+  //   id: 3,
+  //   email: "kumararvind60700@gmail.com",
+  //   experience: "fewrfwe",
+  //   name: "class 1",
+  //   role: "ye3ruerhfou",
+  //   salary: "raj8764654",
+  // },
+  //  {
+  //   id: 4,
+  //   email: "kumararvind60700@gmail.com",
+  //   experience: "fewrfwe",
+  //   name: "class 1",
+  //   role: "ye3ruerhfou",
+  //   salary: "raj8764654",
+  // },
+  //  {
+  //   id: 5,
+  //   email: "kumararvind60700@gmail.com",
+  //   experience: "fewrfwe",
+  //   name: "class 1",
+  //   role: "ye3ruerhfou",
+  //   salary: "raj8764654",
+  // },
+  
+];
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   empList.push({
+    id:empList.length+1, 
     name: empName.value,
     email: email.value,
     salary: salary.value,
@@ -31,6 +102,14 @@ submitBtn.addEventListener("click", (e) => {
     experience: experience.value,
   });
   console.log(empList);
+  renderEmpList({
+    id:empList.length, 
+    name: empName.value,
+    email: email.value,
+    salary: salary.value,
+    role: role.value,
+    experience: experience.value,
+  });
   empName.value = "";
   email.value = "";
   salary.value = "";
